b4a855e9eaf1d1b19eb6dc0db0cfbcaa534cf83a
hels1dee <geminibitly@gmail.com>
2025-06-06 13:56:10 +0300

    Update test_codeMistake.py

--- T11/test_codeMistake.py
+++ T11/test_codeMistake.py
@@ -1,12 +1,10 @@
-# factorial with error
 def factorial(n):
     result = 1
-    for i in range(1, n):  
+    for i in range(1, n + 1):
         result *= i
     return result
-print(factorial(5))  # 24
+print(factorial(5))
 
-# check_password
 def check_password(password):
     if len(password) < 8:                  
         return "Слишком короткий!"          
@@ -16,11 +14,10 @@ def check_password(password):
         return "Пароль надёжен!" 
 print(check_password("qwerty"))
 
-# calculate_sum with error
 def calculate_sum(arr):
     total = 0
-    for i in range(0, len(arr) + 1):  
+    for i in range(0, len(arr)):
         total += arr[i]
     return total
 numbers = [10, 20, 30]
-print(calculate_sum(numbers))  # IndexError
+print(calculate_sum(numbers))
